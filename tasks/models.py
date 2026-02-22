9f1dbbf0c425ec79604c77642f2f38a62f50252c
ankit <dubeysk2003@gmail.com>
2026-02-22 22:10:10 +0530

    able to generate task

--- tasks/models.py
+++ tasks/models.py
@@ -21,5 +21,5 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    def __str__ (self):
+    def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
+from .forms import TaskForm
+
 
 @login_required
 def dashboard(request):
     tasks = Task.objects.filter(assignee=request.user)
     
     todo_tasks = tasks.filter(status='todo')
-    inprogress_tasks = tasks.filter(status='inprogress')
+    inprogress_tasks = tasks.filter(status='in_progress')
     done_tasks = tasks.filter(status='done')
     
     context = {
@@ -17,4 +19,18 @@ def dashboard(request):
     }
     
     return render(request, 'dashboard.html', context)
+
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.assignee = request.user
+            task.save()
+            return redirect('dashboard')
+    else:
+        form = TaskForm()
+        
+    return render(request, 'create_task.html', {'form': form})
 
