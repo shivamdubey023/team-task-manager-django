9bfd891076e2da2f03222087246bc2f51e11234c
ankit <dubeysk2003@gmail.com>
2026-02-22 20:30:01 +0530

    DB model created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+    
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_tasks')
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__ (self):
+        return self.title
